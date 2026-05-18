e6e7ae306beaed241a1b811e113ef49ed244bbcd
iishkaQA <thisiscookie18@yandex.ru>
2026-05-18 12:21:16 +0300

    Update ParcelService.java

--- src/delivery/service/ParcelService.java
+++ src/delivery/service/ParcelService.java
@@ -1,1 +1,19 @@
+package delivery.service;
 
+import delivery.model.Parcel;
+
+public class ParcelService {
+
+    public void printParcelsReport(Parcel[] parcels) {
+
+        for (Parcel parcel : parcels) {
+
+            parcel.printInfo();
+
+            System.out.println("Price: "
+                    + parcel.calculateDeliveryPrice());
+
+            System.out.println();
+        }
+    }
+}

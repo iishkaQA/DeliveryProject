f1401b57ae63dbedaeeda4948c2f71701c23bd31
iishkaQA <thisiscookie18@yandex.ru>
2026-05-18 11:54:14 +0300

    Update Main.java

--- src/delivery/app/Main.java
+++ src/delivery/app/Main.java
@@ -1,1 +1,43 @@
+package delivery.app;
 
+import delivery.model.Parcel;
+import delivery.model.FragileParcel;
+import delivery.model.ExpressParcel;
+import delivery.service.ParcelService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Parcel p1 = new Parcel(
+                "Ivan",
+                "Moscow",
+                2,
+                "111"
+        );
+
+        FragileParcel p2 = new FragileParcel(
+                "Anna",
+                "Kazan",
+                1.5,
+                "222",
+                true
+        );
+
+        ExpressParcel p3 = new ExpressParcel(
+                "Petr",
+                "Sochi",
+                3,
+                "333",
+                12
+        );
+
+        Parcel p4 = new Parcel();
+
+        Parcel[] parcels = {p1, p2, p3, p4};
+
+        ParcelService service = new ParcelService();
+
+        service.printParcelsReport(parcels);
+    }
+}

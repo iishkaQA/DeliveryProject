ed43533c90028ac6135376a36ac30cb9c2506279
iishkaQA <thisiscookie18@yandex.ru>
2026-05-18 11:57:31 +0300

    Update FragileParcel.java

--- src/delivery/model/FragileParcel.java
+++ src/delivery/model/FragileParcel.java
@@ -1,1 +1,30 @@
+package delivery.model;
 
+public class FragileParcel extends Parcel {
+
+    private boolean requiresCarefulHandling;
+
+    public FragileParcel(String name,
+                         String address,
+                         double weight,
+                         String trackNumber,
+                         boolean requiresCarefulHandling) {
+
+        super(name, address, weight, trackNumber);
+
+        this.requiresCarefulHandling = requiresCarefulHandling;
+    }
+
+    @Override
+    public double calculateDeliveryPrice() {
+        return super.calculateDeliveryPrice() + 200;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+
+        System.out.println("Handle with care: "
+                + requiresCarefulHandling);
+    }
+}
